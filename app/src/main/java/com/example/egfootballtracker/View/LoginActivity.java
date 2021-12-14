ac18c200c7c64f28a88bb7eababf31d446bd4f9b
Arkam <arkam.ardsb@gmail.com>
2021-12-14 15:06:27 +0530

    register and login page completed

--- app/src/main/java/com/example/egfootballtracker/View/LoginActivity.java
+++ app/src/main/java/com/example/egfootballtracker/View/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.egfootballtracker;
+package com.example.egfootballtracker.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.egfootballtracker.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
